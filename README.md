f2aff20255643066e8741d60cf970c876112e489
caihanwu <caihanwu@shandagames.com>
2018-12-05 17:26:18 +0800

    update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # wrap
-include compliler, linker, run time wraper
+  include compliler, linker, run time wraper
+
+## compiling
+[caihanwu: ~/workspace/cpp/test/wrap]$ gcc -DCOMPILETIME -c mymalloc.c 
+[caihanwu: ~/workspace/cpp/test/wrap]$ gcc -I. -o intc int.c mymalloc.o
+[caihanwu: ~/workspace/cpp/test/wrap]$ ./intc
+
+## linking
+[caihanwu: ~/workspace/cpp/test/wrap]$ gcc -DLINKTIME -c mymallo.c
+[caihanwu: ~/workspace/cpp/test/wrap]$ gcc -c int.c
+[caihanwu: ~/workspace/cpp/test/wrap]$ gcc -Wl, --wrap,malloc -Wl,--wrap,free -o intl int.o mymalloc.o
+[caihanwu: ~/workspace/cpp/test/wrap]$ ./intl
+
+## running
+[caihanwu: ~/workspace/cpp/test/wrap]$ gcc -DRUNTIME -shared -fpic -o mymalloc.so mymalloc.c -ldl
+[caihanwu: ~/workspace/cpp/test/wrap]$ gcc -o intr int.c
+[caihanwu: ~/workspace/cpp/test/wrap]$ LD_PRELOAD="./mymalloc.so" ./intr
+
---
+++ mymalloc.c
@@ -0,0 +1,75 @@
+#ifdef COMPILETIME
+
+#include <stdio.h>
+#include <malloc.h>
+
+void *mymalloc(size_t size){
+	void *ptr = malloc(size);
+	printf("mymalloc (%d) = %p\n", (int)size, ptr);
+	return ptr;
+}
+
+void myfree(void *ptr){
+	free(ptr);
+	printf("myfree (%p)\n", ptr);
+}
+#endif
+
+#ifdef LINKTIME
+#include <stdio.h>
+
+void *__real_malloc(size_t size);
+void __real_free(void *ptr);
+
+void *__wrap_malloc(size_t size){
+	void *ptr = __real_malloc(size);  /* Call libc malloc */
+	printf("warp_malloc (%d) = %p\n", (int)size, ptr);
+	return ptr;
+}
+
+void __wrap_free(void *ptr){
+	__real_free(ptr);
+	printf("__wrap_free(%p)\n", ptr);
+}
+
+#endif
+
+#ifdef RUNTIME
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+void *malloc(size_t size){
+	void *(*mallocp)(size_t size);
+	char *error;
+
+	mallocp = dlsym(RTLD_NEXT, "malloc");
+	if ((error = dlerror()) != NULL){
+		fputs(error, stderr);
+		exit(1);
+	}
+
+	char *ptr = mallocp(size);
+	printf("dl malloc(%d) = %p\n", (int)size, ptr);
+	return ptr;
+}
+
+void free(void *ptr){
+	void (*freep)(void *) = NULL;
+	char *error;
+
+	if (!ptr)
+		return;
+
+	freep = dlsym(RTLD_NEXT, "free");
+	if ((error = dlerror()) != NULL){
+		fputs(error, stderr);
+		exit(1);
+	}
+
+	freep(ptr);
+	printf("dl free(%p)\n", ptr);
+}
+
+#endif
